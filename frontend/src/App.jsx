db16596eabc8d9e1ccf48c2ca7b4599fff8e992c
justpramod <adhikaripramod864@gmail.com>
2026-09-16 23:59:44 +0545

    feat: Add frontend scaffold and initial components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+    Check
+   </>
+  )
+}
+export default App
---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,12 @@
+import axiosClient from "./axiosClient";
+
+export const registerUser = async (userData)=>{
+
+    const res = await axiosClient.post('auth/register', userData);
+    return res.data
+};
+
+export const loginUser = async (Credential)=>{
+    const res = await axiosClient.post('auth/login', Credential);
+    return res.data
+}
