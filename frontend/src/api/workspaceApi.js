ffd06104464b477e4e489b285d3fd5ac8020f870
justpramod <adhikaripramod864@gmail.com>
2026-09-25 19:26:17 +0545

    feat: add createWorkspace axios

--- frontend/src/api/workspaceApi.js
+++ frontend/src/api/workspaceApi.js
@@ -5,3 +5,9 @@ export const getWorkspace = async()=>{
     const res = await axiosClient.get('/workspace');
     return res.data;
 } 
+
+export const createWorkspace = async(name)=>{
+    const res = await axiosClient.post('/workspace', name);
+    return res.data;
+
+}
