0030b04dac428cd5e008d6c5fe51122c62b39701
justpramod <adhikaripramod864@gmail.com>
2026-09-26 15:23:06 +0545

    fix(workspaces): request all dashboard worksapces via paginated API params

--- frontend/src/api/workspaceApi.js
+++ frontend/src/api/workspaceApi.js
@@ -1,13 +1,12 @@
 import axiosClient from "./axiosClient";
 
-export const getWorkspace = async()=>{
-
-    const res = await axiosClient.get('/workspace');
+export const getWorkspace = async (params = {}) => {
+    const res = await axiosClient.get('/workspace', { params });
     return res.data;
 } 
 
-export const createWorkspace = async(name)=>{
-    const res = await axiosClient.post('/workspace', name);
+export const createWorkspace = async({name})=>{
+    const res = await axiosClient.post('/workspace', {name});
     return res.data;
 
 }
